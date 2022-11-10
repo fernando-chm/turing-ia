1e3444341017de0cbbb5174db2ff2b68cd29231e
Fernando Ch <fernando.chm16@gmail.com>
2022-11-10 12:12:04 -0600

    bugs fixed

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -3,6 +3,9 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const closeHamburger = document.querySelector('.close-hamburger');
 const body = document.getElementById('body');
+const html = document.documentElement;
+const contact = document.getElementById('contact');
+const siteHeight = screen.height;
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.replace('no-display', 'display-block');
@@ -17,4 +20,12 @@ closeHamburger.addEventListener('click', () => {
     logo.classList.replace('no-display', 'display-block');
     hamburger.classList.replace('no-display', 'display-block');
     body.removeAttribute('style');
+});
+
+contact.addEventListener('click', () => {
+    navLinks.classList.replace('display-block', 'no-display');
+    logo.classList.replace('no-display', 'display-block');
+    hamburger.classList.replace('no-display', 'display-block');
+    body.removeAttribute('style');
+    html.scrollTop = ((siteHeight*12) - (siteHeight/2));
 });
